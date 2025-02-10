8f993c63b8b4d5bac76a1f5932c164c0b394f20e
romnick <romnicksanjuan9@gmail.com>
2025-02-10 18:07:40 +0800

    3nd

--- frontend/src/components/Pin.tsx
+++ frontend/src/components/Pin.tsx
@@ -42,23 +42,23 @@ const Pin = () => {
 
     return (
         <div className='bg-gray-100 flex justify-center items-center w-[100%] min-h-screen h-auto'>
-            <div className='bg-white w-[70%] h-auto rounded-lg justify-center border-4 border-pink-500 items-center p-4 sm:w-[90%] md:w-[80%] h-100 xl:w-[50%]'>
+            <div className='bg-white w-[70%] h-auto rounded-lg justify-center border-4 border-pink-400 items-center p-4 sm:w-[90%] md:w-[80%] h-100 xl:w-[50%]'>
                 <input type="number" value={pin.join('')} className={`w-full h-10 bg-pink-500 rounded-lg text-white p-2 text-center ${errorPinMessage ? 'border-2 border-red-500' : ''}`} disabled placeholder='Enter Pin' />
                 {errorPinMessage && <p className='m-0 p-0 text-red-500 text-xs text-center'>{errorPinMessage}</p>}
                 <div className='flex justify-center w-full mt-4'>
                     <div className='grid grid-cols-3 gap-7'>
-                        <button onClick={() => handlePin(1)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>1</button>
-                        <button onClick={() => handlePin(2)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>2</button>
-                        <button onClick={() => handlePin(3)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>3</button>
-                        <button onClick={() => handlePin(4)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>4</button>
-                        <button onClick={() => handlePin(5)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>5</button>
-                        <button onClick={() => handlePin(6)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>6</button>
-                        <button onClick={() => handlePin(7)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>7</button>
-                        <button onClick={() => handlePin(8)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>8</button>
-                        <button onClick={() => handlePin(9)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>9</button>
-                        <button onClick={() => handlePin(10)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white opacity-0'></button>
-                        <button onClick={() => handlePin(0)} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>0</button>
-                        <button onClick={() => handleX()} className='bg-blue-400 h-10 w-11 rounded-2xl text-white'>x</button>
+                        <button onClick={() => handlePin(1)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>1</button>
+                        <button onClick={() => handlePin(2)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>2</button>
+                        <button onClick={() => handlePin(3)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>3</button>
+                        <button onClick={() => handlePin(4)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>4</button>
+                        <button onClick={() => handlePin(5)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>5</button>
+                        <button onClick={() => handlePin(6)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>6</button>
+                        <button onClick={() => handlePin(7)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>7</button>
+                        <button onClick={() => handlePin(8)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>8</button>
+                        <button onClick={() => handlePin(9)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>9</button>
+                        <button onClick={() => handlePin(10)} className='bg-red-400 h-10 w-11 rounded-2xl text-white opacity-0'></button>
+                        <button onClick={() => handlePin(0)} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>0</button>
+                        <button onClick={() => handleX()} className='bg-red-400 h-10 w-11 rounded-2xl text-white'>x</button>
                     </div>
                 </div>
             </div>
